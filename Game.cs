93bb6c8275695ab905e28e9bce64532f6190d068
BuildTools <unconfigured@null.spigotmc.org>
2025-03-05 10:19:17 +0100

    first commit

---
+++ Game.cs
@@ -0,0 +1,29 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace MrEllen.ZPG 
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() 
+        { 
+            Size = (width, height), Title = title 
+        }) 
+        {
+
+        }
+    
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            
+            if (KeyboardState.IsKeyDown(Keys.Escape)) 
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MrEllen.ZPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(800, 600, "ZPG");
+            game.Run();
+        }
+    }
+}
