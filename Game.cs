be511f7722db1d04bfa87ed7a432008dc2e5b628
BuildTools <unconfigured@null.spigotmc.org>
2025-03-07 10:26:32 +0100

    Added basic structure for Game.cs

--- Game.cs
+++ Game.cs
@@ -14,8 +14,25 @@ namespace MrEllen.ZPG
         {
 
         }
-    
 
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            //Code goes here
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            SwapBuffers();
+        }
+    
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
@@ -24,6 +41,13 @@ namespace MrEllen.ZPG
             {
                 Close();
             }
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
     }
 }
